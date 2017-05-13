cd9dddde7ccda559472d1ef856b3351e5cd91b3f
n-freitas <n_freitas1@salemstate.edu>
2017-05-13 11:37:52 -0400

    change dbname and host to process env

--- API/config/sequelize-config.js
+++ API/config/sequelize-config.js
@@ -2,8 +2,8 @@ module.exports = {
   development: {
       username: process.env.RAFR_DB_USERNAME,
       password: process.env.RARF_DB_PASSWORD,
-      database: "rafr",
-      host: "localhost",
+      database: process.env.RERF_DB_NAME,
+      host: process.env.RAFR_DB_HOST,
       dialect: "mysql"
   }
 };
