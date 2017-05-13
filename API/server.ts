6d43b27c106516da042e480b952f03a9773950f4
n-freitas <n_freitas1@salemstate.edu>
2017-05-13 13:57:47 -0400

    add response logging

--- API/server.ts
+++ API/server.ts
@@ -1,4 +1,5 @@
 import * as hapi from "hapi";
+import * as good from 'good';
 import {inject, injectable} from "inversify";
 
 import {AppConfig} from "./config/app-config";
@@ -27,13 +28,43 @@ export class Server {
             port: this.appConfig.server.portNumber
         });
 
+        // add logging
+        await this.registerLogger();
+
         // start the server server
         return new Promise((resolve, reject) => this.server.start(err => {
             if (err) {
                 return reject(err);
             }
 
-            return resolve();
+            resolve();
+        }));
+    }
+
+    /**
+     * Adds logging to stdout for responses
+     * @returns {Promise<T>}
+     */
+    private async registerLogger() {
+        return new Promise((resolve, reject) => this.server.register({
+            register: good,
+            options: {
+                reporters: {
+                    myConsoleReporter: [{
+                        module: 'good-squeeze',
+                        name: 'Squeeze',
+                        args: [{log: '*', response: '*'}]
+                    }, {
+                        module: 'good-console'
+                    }, 'stdout']
+                }
+            }
+        }, err => {
+            if (err) {
+                return reject(err);
+            }
+
+            resolve();
         }));
     }
 }
