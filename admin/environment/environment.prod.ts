4be0268808d0981cf34175046c80a0643d549781
Nickolas Freitas <nickolas@nickolasfreitas.com>
2017-05-15 23:17:33 -0400

    merge environment files

--- admin/environment/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  envName: `prod`,
-  apiUrl: `http://ec2-54-82-207-216.compute-1.amazonaws.com:3000/`
-};
--- admin/environment/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: false,
-  envName: `dev`,
-  apiUrl: `http://localhost:3000/`
-};
