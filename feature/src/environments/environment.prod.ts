0ec601cccd2ec8b15b1194f12ca23d30bf46d97a
Nickolas Freitas <nickolas@nickolasfreitas.com>
2017-05-21 21:15:01 -0400

    update api url

--- feature/src/environments/environment.prod.ts
+++ feature/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   envName: `prod`,
-  apiUrl: `http://ec2-54-82-207-216.compute-1.amazonaws.com:3000/`
+  apiUrl: `http://ec2-34-207-239-7.compute-1.amazonaws.com:3000/`
 };
